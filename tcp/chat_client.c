a6727f7fb74ee2a54c69980828d79dcdfb89df2a
jimtran <jim.tran.287@gmail.com>
2018-01-15 11:33:50 +0700

    Update chat_client.c

--- tcp/chat_client.c
+++ tcp/chat_client.c
@@ -5,39 +5,107 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#define LENGTH 512
+
+void sendFile(int serv){
+    char* fs_name = "/Users/Jim/Desktop/ltb.txt";
+    char sdbuf[LENGTH];
+    printf("[Client] Sending %s to the Server... ", fs_name);
+    FILE *fs = fopen(fs_name, "r");
+    if(fs == NULL)
+    {
+        printf("ERROR: File %s not found.\n", fs_name);
+        exit(1);
+    }
+    
+    bzero(sdbuf, LENGTH);
+    int fs_block_sz;
+    while((fs_block_sz = fread(sdbuf, sizeof(char), LENGTH, fs)) > 0)
+    {
+        if(send(serv, sdbuf, fs_block_sz, 0) < 0)
+        {
+            fprintf(stderr, "ERROR: Failed to send file %s. (errno = %d)\n", fs_name, errno);
+            break;
+        }
+        bzero(sdbuf, LENGTH);
+    }
+    printf("Ok File %s from Client was Sent!\n", fs_name);
+}
+void receiveFile(int serv){
+    char revbuf[LENGTH];
+    printf("[Client] Receiveing file from Server and saving it as final.txt...");
+    char* fr_name = "/Users/Jim/Desktop/dat.txt";
+    FILE *fr = fopen(fr_name, "a");
+    if(fr == NULL)
+        printf("File %s Cannot be opened.\n", fr_name);
+    else
+    {
+        bzero(revbuf, LENGTH);
+        int fr_block_sz = 0;
+        while((fr_block_sz = recv(serv, revbuf, LENGTH, 0)) > 0)
+        {
+            int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
+            bzero(revbuf, LENGTH);
+            if (fr_block_sz == 0 || fr_block_sz != 512)
+            {
+                break;
+            }
+        }
+        if(fr_block_sz < 0)
+        {
+            if (errno == EAGAIN)
+            {
+                printf("recv() timed out.\n");
+            }
+            else
+            {
+                fprintf(stderr, "recv() failed due to errno = %d\n", errno);
+            }
+        }
+        printf("Ok received from server!\n");
+        fclose(fr);
+    }
+    close (serv);
+    printf("[Client] Connection lost.\n");
+}
+
 
 int main(int argc, char* argv[]) {
     int so;
     char s[100];
     struct sockaddr_in ad;
-
+    
     socklen_t ad_length = sizeof(ad);
     struct hostent *hep;
-
+    
     // create socket
     int serv = socket(AF_INET, SOCK_STREAM, 0);
-
+    
     // init address
     hep = gethostbyname(argv[1]);
     memset(&ad, 0, sizeof(ad));
     ad.sin_family = AF_INET;
-    ad.sin_addr = *(struct in_addr *)hep->h_addr_list[0];
+    ad.sin_addr = *((struct in_addr *)hep->h_addr);
     ad.sin_port = htons(12345);
-
+    
     // connect to server
     connect(serv, (struct sockaddr *)&ad, ad_length);
-
+    
     while (1) {
         // after connected, it's client turn to chat
-
+        
         // send some data to server
-        printf("client>");
-        scanf("%s", s);
-        write(serv, s, strlen(s) + 1);
-
+        //        printf("client>");
+        //        scanf("%s", s);
+        //        write(serv, s, strlen(s) + 1);
+        
+        sendFile(serv);
+        
         // then it's server turn
-        read(serv, s, sizeof(s));
-
+//        read(serv, s, sizeof(s));
+        receiveFile(serv);
         printf("server says: %s\n", s);
     }
 }
